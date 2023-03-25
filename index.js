58a63436b84e907093ced55d151d6b45f1355c1f
Alco2 <abdou.lahoub.wwe@gmail.com>
2023-03-25 14:02:34 +0000

    fixed navbar bug

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ let openNav = document.getElementById('openNav');
 let navbar = document.getElementById('navright');
 let closeNav = document.getElementById('closeNav');
 
+function handleNavbarState() {
+    if (window.innerWidth <= 600) {
+      navbar.style.transform = 'translateX(100%)';
+      closeNav.style.display = 'block';
+    } else {
+      navbar.style.transform = 'translateX(0%)';
+      closeNav.style.display = 'none';
+    }
+  }
+
+handleNavbarState();
+window.addEventListener('resize', handleNavbarState);
 
 openNav.onclick = ()=>{
     navbar.style.transform = 'translateX(0)'
+    closeNav.style.display = 'block'
 }
 closeNav.onclick = ()=>{
     navbar.style.transform = 'translateX(100%)'
+    closeNav.style.display = 'none'
 }
